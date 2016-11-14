9591363ae90c720e0e73f7e09741923b387190a7
kropladev <makroplewski@gmail.com>
2016-11-14 07:11:29 +0100

    adding removed AppConfig - bugfix

---
+++ src/main/java/com/nordea/assignment/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.nordea.assignment.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by kropla on 11.11.16.
+ */
+@Configuration
+@ComponentScan("com.nordea.assignment")
+public class AppConfig {
+}
